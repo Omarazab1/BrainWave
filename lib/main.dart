d1163bc5fec11473e4e00b25e6f2a63e73093a61
Omarazab1 <oazab6806@gmail.com>
2024-07-28 01:47:14 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const NotesApp());
+}
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notes App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const NotesScreen(),
+    );
+  }
+}

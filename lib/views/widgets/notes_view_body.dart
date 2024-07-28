d1fed49425cad2d58988d9d3b142369a1b00b992
Omarazab1 <oazab6806@gmail.com>
2024-07-28 05:17:24 +0300

    create notesListView and floating action button

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notesapp/views/widgets/custom_appbar.dart';
-import 'package:notesapp/views/widgets/custom_note_item.dart';
+import 'package:notesapp/views/widgets/notes_listview.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -14,7 +14,7 @@ class NotesViewBody extends StatelessWidget {
           SizedBox(height: 10,),
           CustomAppbar(),
           SizedBox(height: 10,),
-          CustomNoteItem(),
+          NotesListview(),
         ],
       ),
     );

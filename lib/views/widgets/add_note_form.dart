a6b2aefe87490e3d9756993d30c46f358466b015
Omarazab1 <oazab6806@gmail.com>
2024-07-30 03:30:46 +0300

    change add Note Loading

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notesapp/cubits/add_note_cubit/add_note_cubit.dart';
 
+import '../../cubits/add_note_cubit/add_note_state.dart';
 import '../../models/note_model.dart';
 import 'custom_button.dart';
 import 'custom_textfield.dart';
@@ -49,21 +50,26 @@ class _AddNoteFormState extends State<AddNoteForm> {
           const SizedBox(
             height: 26,
           ),
-          CustomButton(
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-                var noteModel = NoteModel(
-                    title: title!,
-                    content: subtitle!,
-                    date: DateTime.now().toString(),
-                    color: Colors.blue.value);
-                BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
-              } else {
-                setState(() {
-                  autovalidateMode = AutovalidateMode.always;
-                });
-              }
+          BlocBuilder<AddNoteCubit, AddNoteState>(
+            builder: (context,state) {
+              return CustomButton(
+                isLoading: state is AddNoteLoading ? true : false,
+                onTap: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                    var noteModel = NoteModel(
+                        title: title!,
+                        content: subtitle!,
+                        date: DateTime.now().toString(),
+                        color: Colors.blue.value);
+                    BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
+                  } else {
+                    setState(() {
+                      autovalidateMode = AutovalidateMode.always;
+                    });
+                  }
+                },
+              );
             },
           ),
           const SizedBox(
--- lib/views/widgets/custom_modal_bottom_sheet.dart
+++ lib/views/widgets/custom_modal_bottom_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import '../../cubits/add_note_cubit/add_note_cubit.dart';
 import '../../cubits/add_note_cubit/add_note_state.dart';
 import 'add_note_form.dart';
@@ -12,27 +11,27 @@ class CustomModalBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return  BlocProvider(
       create: (context) => AddNoteCubit(),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12.0),
-        child:  BlocConsumer<AddNoteCubit,AddNoteState>(
-          listener: (context , state){
-            if(state is AddNoteError){
-              ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text(state.errorMessage))
-              );
-            }
-            if(state is AddNoteSuccess){
-              Navigator.pop(context);
-            }
-          },
-          builder: (context , state ){
-            return  ModalProgressHUD(
-                inAsyncCall: state is AddNoteLoading ? true : false,
-                child:const SingleChildScrollView(child:  AddNoteForm()),
+      child: BlocConsumer<AddNoteCubit,AddNoteState>(
+        listener: (context , state){
+          if(state is AddNoteError){
+            ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(state.errorMessage))
             );
-          },
+          }
+          if(state is AddNoteSuccess){
+            Navigator.pop(context);
+          }
+        },
+        builder: (context , state ){
+          return  AbsorbPointer(
+            absorbing: state is AddNoteLoading ? true : false,
+            child: const Padding(
+              padding:  EdgeInsets.symmetric(horizontal: 16.0),
+              child:  SingleChildScrollView(child:  AddNoteForm()),
+            ),
+          );
+        },
 
-        ),
       ),
     );
   }
